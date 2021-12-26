06b0d579c1a833a4d8a364b461583e98272701ff
rconjoe <joe@rconjoe.com>
2021-12-26 10:02:05 -0800

    replace links for testimonial

--- aali/src/components/layouts/Header.vue
+++ aali/src/components/layouts/Header.vue
@@ -16,7 +16,7 @@
             <li><a href="#about">About</a></li>
             <!-- <li><a href="#portfolio">Portfolio</a></li> -->
             <li><a href="#service">Services</a></li>
-            <!-- <li><a href="#testimonial">Testimonial</a></li> -->
+            <li><a href="#testimonial">Testimonial</a></li>
             <li><a href="#blog">Blog</a></li>
             <li><a href="#contact">Contact</a></li>
           </ul>
