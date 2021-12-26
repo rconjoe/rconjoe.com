852f2ac65864f35ac3687dcb88aa044a5831b2ea
rconjoe <joe@rconjoe.com>
2021-12-25 21:41:26 -0800

    fix nonexistent mobile routes

--- aali/src/components/layouts/MobileMenu.vue
+++ aali/src/components/layouts/MobileMenu.vue
@@ -23,9 +23,9 @@
         <ul class="anchor_nav">
           <li class="current"><a href="#home">Home</a></li>
           <li><a href="#about">About</a></li>
-          <li><a href="#portfolio">Portfolio</a></li>
+          <!-- <li><a href="#portfolio">Portfolio</a></li> -->
           <li><a href="#service">Service</a></li>
-          <li><a href="#testimonial">Testimonial</a></li>
+          <!-- <li><a href="#testimonial">Testimonial</a></li> -->
           <li><a href="#blog">Blog</a></li>
           <li><a href="#contact">Contact</a></li>
         </ul>
